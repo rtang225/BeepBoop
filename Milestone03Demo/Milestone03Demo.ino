f43b4bd7250cef8fcfe5e77ae63bff3cab3bbd7b
Wonger123 <andrewwong817@gmail.com>
2024-03-13 21:02:28 -0400

    Cleaned code, made spiral pattern

--- Milestone03Demo/Milestone03Demo.ino
+++ Milestone03Demo/Milestone03Demo.ino
@@ -1,38 +1,8 @@
 /*
-
- MSE 2202 MSEBot base code for Lab 4
+ MSE 2202 Milestone 03 drive system video demo code
  Language: Arduino
- Authors: Eugen Porter and Michael Naish
- 
- Rev 1 - Initial version 2016
- Rev 2 - Update for MSEduino V0.2
- ...
- Rev 4 - revisit for MSEduino V4.2 2023
- Rev 5 - revisit for MSEduino V5.1 2024
-
+ Authors: Andrew Wong
  */
-
-//  To program and use ESP32-S3
-//
-//  Tools->:
-//  Board: "Adafruit Feather ESP32-S3 No PSRAM"
-//  Upload Speed: "921600"
-//  USB CDC On Boot: "Enabled"
-//  USB Firmware MSC on Boot: "Disabled"
-//  USB DFU On Bot: "Disabled"
-//  Upload Mode:"UART0/Hardware CDC"
-//  SPU Frequency: "240MHz (WiFi)"
-//  Flash Mode: "QIO 80MHz"
-//  Flash SIze: "4MB (32Mb)"
-//  Partition Scheme: "Default 4MB with spiffs (1.2MB app/1.5MB SPIFFS)"
-//  Core Debug Level: "Verbose"
-//  PSRAM: 'Disabled"
-//  Arduino Runs On: "Core 1"
-//  Events Run On: "Core 1"
-//
-//  To program, press and hold the reset button then press and hold program button, release the reset button then
-//  release the program button
-//
 
 // Uncomment keywords to enable debugging output
 // #define DEBUG_DRIVE_SPEED    1
@@ -57,12 +27,12 @@ void setTarget(int dir, long pos, double dist);  // sets encoder position target
 #define ENCODER_RIGHT_B 12     // right encoder B signal is connected to pin 20 GPIO12 (J12)
 #define MODE_BUTTON 0          // GPIO0  pin 27 for Push Button 1
 #define MOTOR_ENABLE_SWITCH 3  // DIP Switch S1-1 pulls Digital pin D3 to ground when on, connected to pin 15 GPIO3 (J3)
-#define POT_R1 1               // when DIP Switch S1-3 is on, Analog AD0 (pin 39) GPIO1 is connected to Poteniometer R1
-#define SMART_LED 21           // when DIP Switch S1-4 is on, Smart LED is connected to pin 23 GPIO21 (J21)
-#define SMART_LED_COUNT 1      // number of SMART LEDs in use
-#define IR_DETECTOR 14         // GPIO14 pin 17 (J14) IR detector input
-#define SHOULDER_SERVO 41      // GPIO41 pin 34 (J41) Servo 1
-#define CLAW_SERVO 42          // GPIO42 pin 35 (J42) Servo 2
+// #define POT_R1 1               // when DIP Switch S1-3 is on, Analog AD0 (pin 39) GPIO1 is connected to Poteniometer R1
+#define SMART_LED 21       // when DIP Switch S1-4 is on, Smart LED is connected to pin 23 GPIO21 (J21)
+#define SMART_LED_COUNT 1  // number of SMART LEDs in use
+// #define IR_DETECTOR 14         // GPIO14 pin 17 (J14) IR detector input
+// #define SHOULDER_SERVO 41      // GPIO41 pin 34 (J41) Servo 1
+// #define CLAW_SERVO 42          // GPIO42 pin 35 (J42) Servo 2
 
 // Constants
 const int cDisplayUpdate = 100;           // update interval for Smart LED in milliseconds
@@ -96,8 +66,6 @@ unsigned char leftDriveSpeed;         // motor drive speed (0-255)
 unsigned char rightDriveSpeed;        // motor drive speed (0-255)
 unsigned char driveIndex;             // state index for run mode
 unsigned int modePBDebounce;          // pushbutton debounce timer count
-unsigned int potClawSetpoint;         // desired position of claw servo read from pot
-unsigned int potShoulderSetpoint;     // desired position of shoulder servo read from pot
 unsigned long timerCount3sec = 0;     // 3 second timer count in milliseconds
 unsigned long timerCount2sec = 0;     // 2 second timer count in milliseconds
 unsigned long timerCount200msec = 0;  // 200 millisecond timer count in milliseconds
@@ -106,14 +74,9 @@ unsigned long previousMicros;         // last microsecond count
 unsigned long currentMicros;          // current microsecond count
 double target;                        // target encoder count to keep track of distance travelled
 unsigned long prevTime;               // Get the current time in milliseconds
-int armServoTopValue = 2100;
-int armServoBottomValue = 1000;
-int armServoPos = 2000;
-int clawServoPos;
-char irData = ' ';
-boolean servoUp = false;
-boolean clawOpen = false;
-boolean beaconFound = false;
+float driveDistance = 25;             // Forward/backward drive distance
+float turningDistance = 8.5;          // Turning distance counter
+int driveCounter = 0;                 // Counter for drive circles
 
 // Declare SK6812 SMART LED object
 //   Argument 1 = Number of LEDs (pixels) in use
@@ -134,19 +97,14 @@ unsigned char LEDBrightnessLevels[] = { 5, 15, 30, 45, 60, 75, 90, 105, 120, 135
 unsigned int robotModeIndex = 0;  // robot operational state
 unsigned int modeIndicator[6] = {
   // colours for different modes
-  SmartLEDs.Color(255, 0, 0),    //   red - stop
-  SmartLEDs.Color(0, 255, 0),    //   green - run
-  SmartLEDs.Color(0, 0, 255),    //   blue - empty case
-  SmartLEDs.Color(255, 255, 0),  //   yellow - empty case
-  SmartLEDs.Color(0, 255, 255),  //   cyan - empty case
-  SmartLEDs.Color(255, 0, 255)   //   magenta - empty case
+  SmartLEDs.Color(255, 0, 0),  //   red - stop
+  SmartLEDs.Color(0, 255, 0),  //   green - run
 };
 
 // Motor, encoder, and IR objects (classes defined in MSE2202_Lib)
 Motion Bot = Motion();               // Instance of Motion for motor control
 Encoders LeftEncoder = Encoders();   // Instance of Encoders for left encoder data
 Encoders RightEncoder = Encoders();  // Instance of Encoders for right encoder data
-IR Scan = IR();                      // instance of IR for detecting IR signals
 
 void setup() {
 #if defined DEBUG_DRIVE_SPEED || DEBUG_ENCODER_COUNT
@@ -155,12 +113,10 @@ void setup() {
 
   // Set up motors and encoders
   Bot.driveBegin("D1", LEFT_MOTOR_A, LEFT_MOTOR_B, RIGHT_MOTOR_A, RIGHT_MOTOR_B);  // set up motors as Drive 1
-  Bot.servoBegin("S1", CLAW_SERVO);                                                // set up claw servo
-  Bot.servoBegin("S2", SHOULDER_SERVO);                                            // set up shoulder servo
-  LeftEncoder.Begin(ENCODER_LEFT_A, ENCODER_LEFT_B, &Bot.iLeftMotorRunning);       // set up left encoder
-  RightEncoder.Begin(ENCODER_RIGHT_A, ENCODER_RIGHT_B, &Bot.iRightMotorRunning);   // set up right encoder
-
-  Scan.Begin(IR_DETECTOR, 1200);  //set up IR Detection @ 1200 baud
+  // Bot.servoBegin("S1", CLAW_SERVO);                                                // set up claw servo
+  // Bot.servoBegin("S2", SHOULDER_SERVO);                                            // set up shoulder servo
+  LeftEncoder.Begin(ENCODER_LEFT_A, ENCODER_LEFT_B, &Bot.iLeftMotorRunning);      // set up left encoder
+  RightEncoder.Begin(ENCODER_RIGHT_A, ENCODER_RIGHT_B, &Bot.iRightMotorRunning);  // set up right encoder
 
   // Set up SmartLED
   SmartLEDs.begin();                                     // initialize smart LEDs object (REQUIRED)
@@ -236,11 +192,6 @@ void loop() {
     // modes
     // 0 = Default after power up/reset. Robot is stopped.
     // 1 = Press mode button once to enter.        Run robot
-    // 2 = Press mode button twice to enter.       Test IR receiver
-    // 3 = Press mode button three times to enter. Test claw servo
-    // 4 = Press mode button four times to enter.  Test shoulder servo
-    // 5 = Press mode button five times to enter.  Add your code to do something
-    // 6 = Press mode button six times to enter.   Add your code to do something
     switch (robotModeIndex) {
       case 0:  // Robot stopped
         Bot.Stop("D1");
@@ -253,7 +204,7 @@ void loop() {
       case 1:              // Run robot
         if (timeUp3sec) {  // pause for 3 sec before running case 1 code
           // Read pot to update drive motor speed
-          pot = analogRead(POT_R1);
+          // pot = analogRead(POT_R1);
           leftDriveSpeed = map(pot, 0, 4095, cMinPWM, cMaxPWM) - cLeftAdjust;
           rightDriveSpeed = map(pot, 0, 4095, cMinPWM, cMaxPWM) - cRightAdjust;
 #ifdef DEBUG_DRIVE_SPEED
@@ -286,16 +237,17 @@ void loop() {
                 case 0:              // Stop
                   Bot.Stop("D1");    // drive ID
 
-                  setTarget(1, RightEncoder.lRawEncoderCount, 5);  // set target to drive forward 45.53cm
-                  driveIndex = 1;                                  // next state: drive forward
+                  setTarget(1, RightEncoder.lRawEncoderCount, driveDistance);  // set target to drive forward
+                  driveIndex++;                                                // next state: drive forward
                   break;
 
                 case 1:                                                // Drive forward
                   Bot.Forward("D1", leftDriveSpeed, rightDriveSpeed);  // drive ID, left speed, right speed
 
+
                   if (RightEncoder.lRawEncoderCount >= target) {
-                    setTarget(-1, RightEncoder.lRawEncoderCount, 9.000);  // set next target to turn 90 degrees CCW
-                    driveIndex = 2;                                       // next state: turn left
+                    setTarget(-1, RightEncoder.lRawEncoderCount, turningDistance);  // set next target to turn 90 degrees CCW
+                    driveIndex++;                                                   // next state: turn left
                   }
                   break;
 
@@ -303,8 +255,9 @@ void loop() {
                   Bot.Left("D1", leftDriveSpeed, rightDriveSpeed);  // drive ID, left speed, right speed
 
                   if (RightEncoder.lRawEncoderCount <= target) {
-                    setTarget(1, RightEncoder.lRawEncoderCount, 10);  // set target to drive forwards 36.735cm
-                    driveIndex = 3;                                   // next state: drive forward
+                    driveDistance += 25;
+                    setTarget(1, RightEncoder.lRawEncoderCount, driveDistance);  // set target to drive forward
+                    driveIndex++;                                                // next state: drive forward
                   }
                   break;
 
@@ -312,61 +265,71 @@ void loop() {
                   Bot.Forward("D1", leftDriveSpeed, rightDriveSpeed);  // drive ID, left speed, right speed
 
                   if (RightEncoder.lRawEncoderCount >= target) {
-                    setTarget(-1, RightEncoder.lRawEncoderCount, 9.000);  // set next target to turn 90 degrees CCW
-                    driveIndex = 4;                                       // next state: localize ping pong ball
+                    setTarget(-1, RightEncoder.lRawEncoderCount, turningDistance);  // set next target to turn 90 degrees CCW
+                    driveIndex++;                                                   // next state: turn left
                   }
                   break;
 
-                case 4:
+                case 4:                                             // Turn left
                   Bot.Left("D1", leftDriveSpeed, rightDriveSpeed);  // drive ID, left speed, right speed
 
                   if (RightEncoder.lRawEncoderCount <= target) {
-                    setTarget(1, RightEncoder.lRawEncoderCount, 15);  // set target to drive forwards 36.735cm
-                    driveIndex = 5;                                   // next state: drive forward
+                    driveDistance += 25;
+                    setTarget(1, RightEncoder.lRawEncoderCount, driveDistance);  // set target to drive forward
+                    driveIndex++;                                                // next state: drive forward
                   }
                   break;
 
-                case 5:
+                case 5:                                                // Drive forward
                   Bot.Forward("D1", leftDriveSpeed, rightDriveSpeed);  // drive ID, left speed, right speed
 
                   if (RightEncoder.lRawEncoderCount >= target) {
-                    setTarget(-1, RightEncoder.lRawEncoderCount, 9.000);  // set next target to turn 90 degrees CCW
-                    driveIndex = 6;                                       // next state: localize ping pong ball
+                    setTarget(-1, RightEncoder.lRawEncoderCount, turningDistance);  // set next target to turn 90 degrees CCW
+                    driveIndex++;                                                   // next state: turn left
                   }
                   break;
 
-                case 6:
+                case 6:                                             // Turn left
                   Bot.Left("D1", leftDriveSpeed, rightDriveSpeed);  // drive ID, left speed, right speed
 
                   if (RightEncoder.lRawEncoderCount <= target) {
-                    setTarget(1, RightEncoder.lRawEncoderCount, 20);  // set target to drive forwards 36.735cm
-                    driveIndex = 7;                                   // next state: drive forward
+                    driveDistance += 25;
+                    setTarget(1, RightEncoder.lRawEncoderCount, driveDistance);  // set target to drive forward
+                    driveIndex++;                                                // next state: drive forward
                   }
                   break;
 
-                case 7:
+                case 7:                                                // Drive forward
                   Bot.Forward("D1", leftDriveSpeed, rightDriveSpeed);  // drive ID, left speed, right speed
 
                   if (RightEncoder.lRawEncoderCount >= target) {
-                    setTarget(-1, RightEncoder.lRawEncoderCount, 9.000);  // set next target to turn 90 degrees CCW
-                    driveIndex = 8;                                       // next state: localize ping pong ball
+                    setTarget(-1, RightEncoder.lRawEncoderCount, turningDistance);  // set next target to turn 90 degrees CCW
+                    driveIndex++;                                                   // next state: turn left
                   }
                   break;
 
-                case 8:
+                case 8:                                             // Turn left
                   Bot.Left("D1", leftDriveSpeed, rightDriveSpeed);  // drive ID, left speed, right speed
 
                   if (RightEncoder.lRawEncoderCount <= target) {
-                    setTarget(1, RightEncoder.lRawEncoderCount, 25);  // set target to drive forwards 36.735cm
-                    driveIndex = 9;                                   // next state: drive forward
+                    driveDistance += 25;
+                    setTarget(1, RightEncoder.lRawEncoderCount, driveDistance);  // set target to drive forward
+                    driveIndex++;                                                // next state: stop robot, exit drive mode
                   }
                   break;
 
-                case 9:
+                case 9:                                                // Drive forward
                   Bot.Forward("D1", leftDriveSpeed, rightDriveSpeed);  // drive ID, left speed, right speed
 
                   if (RightEncoder.lRawEncoderCount >= target) {
-                    robotModeIndex = 0;
+                    if (driveCounter < 3) {
+                      driveCounter++;
+                      driveDistance += 25;
+                      setTarget(1, RightEncoder.lRawEncoderCount, driveDistance);  // set target to drive forward
+                      driveIndex = 1;
+                    } else {
+                      robotModeIndex = 0;
+                    }
                   }
                   break;
               }
@@ -375,47 +338,6 @@ void loop() {
         } else {  // stop when motors are disabled
           Bot.Stop("D1");
         }
-        break;
-
-      case 2:  // Test IR receiver
-        Bot.Stop("D1");
-        if (Scan.Available()) {                // if data is received
-          Serial.println(Scan.Get_IR_Data());  // output received data to serial
-        }
-        break;
-
-      case 3:  // Test claw servo with pot
-        // read pot and map to allowable servo range for claw
-        potClawSetpoint = map(analogRead(POT_R1), 0, 4096, cClawServoClosed, cClawServoOpen);
-        Bot.ToPosition("S1", potClawSetpoint);  // update claw servo position
-        if (timeUp200msec) {                    // limit output rate to serial monitor
-          timeUp200msec = false;                // reset 200 mS timer
-          Serial.print(F("Claw : Pot R1 = "));
-          Serial.print(analogRead(POT_R1));
-          Serial.print(F(", mapped = "));
-          Serial.println(potClawSetpoint);
-        }
-        break;
-
-      case 4:  // Test shoulder servo with pot
-        // read pot and map to allowable stepper range for shoulder
-        potShoulderSetpoint = map(analogRead(POT_R1), 0, 4096, cArmServoDown, cArmServoUp);
-        Bot.ToPosition("S2", potShoulderSetpoint);  // update shoulder servo position
-        if (timeUp200msec) {                        // limit output rate to serial monitor
-          timeUp200msec = false;                    // reset 200 mS timer
-          Serial.print(F("Shoulder : Pot R1 = "));
-          Serial.print(analogRead(POT_R1));
-          Serial.print(F(", mapped = "));
-          Serial.println(potShoulderSetpoint);
-        }
-        break;
-
-      case 5:                //add your code to do something
-        robotModeIndex = 0;  //  !!!!!!!  remove if using the case
-        break;
-
-      case 6:                //add your code to do something
-        robotModeIndex = 0;  //  !!!!!!!  remove if using the case
         break;
     }
 
