b268cf02110709fe988f5f8ab803b94b99938bf0
Andrew Wong <156140260+awong787@users.noreply.github.com>
2024-03-27 19:33:55 -0400

    Created Plan B

--- Plan B/DynamicProgram/DynamicProgram.ino
+++ Plan B/DynamicProgram/DynamicProgram.ino
@@ -1,5 +1,14 @@
-// #define DEBUG_ENCODER_COUNT 1
-#define DEBUG_DRIVE_SPEED 1
+/*
+This is the dynamic program of the project
+
+Controls:
+- Drivetrain
+- Return path
+- Stone dispending
+*/
+
+#define DEBUG_ENCODER_COUNT 1
+// #define DEBUG_DRIVE_SPEED 1
 
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
@@ -10,31 +19,19 @@ void Indicator();                                // for mode/heartbeat on Smart 
 void setTarget(int dir, long pos, double dist);  // sets encoder position target for movement
 
 // Port pin constants
+#define LEFT_MOTOR_A 35        // GPIO35 pin 28 (J35) Motor 1 A
+#define LEFT_MOTOR_B 36        // GPIO36 pin 29 (J36) Motor 1 B
+#define RIGHT_MOTOR_A 37       // GPIO37 pin 30 (J37) Motor 2 A
+#define RIGHT_MOTOR_B 38       // GPIO38 pin 31 (J38) Motor 2 B
+#define ENCODER_LEFT_A 9       // left encoder A signal is connected to pin 8 GPIO15 (J15)
+#define ENCODER_LEFT_B 10      // left encoder B signal is connected to pin 8 GPIO16 (J16)
+#define ENCODER_RIGHT_A 11     // right encoder A signal is connected to pin 19 GPIO11 (J11)
+#define ENCODER_RIGHT_B 12     // right encoder B signal is connected to pin 20 GPIO12 (J12)
 #define MODE_BUTTON 0          // GPIO0  pin 27 for Push Button 1
 #define MOTOR_ENABLE_SWITCH 3  // DIP Switch S1-1 pulls Digital pin D3 to ground when on, connected to pin 15 GPIO3 (J3)
 #define POT_R1 1               // when DIP Switch S1-3 is on, Analog AD0 (pin 39) GPIO1 is connected to Poteniometer R1
 #define SMART_LED 21           // when DIP Switch S1-4 is on, Smart LED is connected to pin 23 GPIO21 (J21)
 #define SMART_LED_COUNT 1      // number of SMART LEDs in use
-
-// Port pin constants for Drive System Wheels
-#define LEFT_MOTOR_A 35     // GPIO35 pin 28 (J35) Motor 1 A
-#define LEFT_MOTOR_B 36     // GPIO36 pin 29 (J36) Motor 1 B
-#define RIGHT_MOTOR_A 37    // GPIO37 pin 30 (J37) Motor 2 A
-#define RIGHT_MOTOR_B 38    // GPIO38 pin 31 (J38) Motor 2 B
-#define ENCODER_LEFT_A 9    // left encoder A signal is connected to GPIO9 (J9)
-#define ENCODER_LEFT_B 10   // left encoder B signal is connected to GPIO10 (J10)
-#define ENCODER_RIGHT_A 11  // right encoder A signal is connected to GPIO11 (J11)
-#define ENCODER_RIGHT_B 12  // right encoder B signal is connected to GPIO12 (J12)
-
-// Port pin constants for Picker Upper Wheel
-#define LEFT_MOTOR_2A 15    // GPIO15 (J15) Motor 1 A
-#define LEFT_MOTOR_2B 16    // GPIO16 (J16) Motor 1 B
-#define RIGHT_MOTOR_2A 17   // GPIO17 (J17) Motor 2 A
-#define RIGHT_MOTOR_2B 18   // GPIO18 (J18) Motor 2 B
-#define ENCODER_LEFT_2A 4   // left encoder A signal is connected to GPIO4 (J4)
-#define ENCODER_LEFT_2B 5   // left encoder B signal is connected to GPIO5 (J5)
-#define ENCODER_RIGHT_2A 6  // right encoder A signal is connected to GPIO6 (J6)
-#define ENCODER_RIGHT_2B 7  // right encoder B signal is connected to GPIO7 (J7)
 
 // Constants
 const int cDisplayUpdate = 100;           // update interval for Smart LED in milliseconds
@@ -66,8 +63,6 @@ boolean timeUp2sec = false;           // 2 second timer elapsed flag
 boolean timeUp200msec = false;        // 200 millisecond timer elapsed flag
 unsigned char leftDriveSpeed;         // motor drive speed (0-255)
 unsigned char rightDriveSpeed;        // motor drive speed (0-255)
-unsigned char wheelLDriveSpeed;       // wheel drive speed (0-255)
-unsigned char wheelRDriveSpeed;       // wheel drive soeed (0-255)
 unsigned char driveIndex;             // state index for run mode
 unsigned int modePBDebounce;          // pushbutton debounce timer count
 unsigned long timerCount3sec = 0;     // 3 second timer count in milliseconds
@@ -110,30 +105,15 @@ Motion Bot = Motion();               // Instance of Motion for motor control
 Encoders LeftEncoder = Encoders();   // Instance of Encoders for left encoder data
 Encoders RightEncoder = Encoders();  // Instance of Encoders for right encoder data
 
-Motion LeftWheel = Motion();          // Instance of Motion for left wheel control
-Motion RightWheel = Motion();         // Instance of Motion for right wheel control
-Encoders LeftEncoder2 = Encoders();   // Instance of Encoders for left encoder data
-Encoders RightEncoder2 = Encoders();  // Instance of Encoders for right encoder data
-
 void setup() {
 #if defined DEBUG_DRIVE_SPEED || DEBUG_ENCODER_COUNT
   Serial.begin(115200);
 #endif
 
-  // Set up bot motors and encoders
+  // Set up motors and encoders
   Bot.driveBegin("D1", LEFT_MOTOR_A, LEFT_MOTOR_B, RIGHT_MOTOR_A, RIGHT_MOTOR_B);  // set up motors as Drive 1
   LeftEncoder.Begin(ENCODER_LEFT_A, ENCODER_LEFT_B, &Bot.iLeftMotorRunning);       // set up left encoder
   RightEncoder.Begin(ENCODER_RIGHT_A, ENCODER_RIGHT_B, &Bot.iRightMotorRunning);   // set up right encoder
-  // leftDriveSpeed = cMaxPWM - cLeftAdjust;                                          // Set left drive motor speed to max
-  // rightDriveSpeed = cMaxPWM - cRightAdjust;                                        // Set right drive motor speed to max
-
-  // Set up wheel motors and encoders
-  LeftWheel.motorBegin("M1", LEFT_MOTOR_2A, LEFT_MOTOR_2B);     // set up motor as Motor 1
-  RightWheel.motorBegin("M1", RIGHT_MOTOR_2A, RIGHT_MOTOR_2B);  // set up motor as Motor 2
-  // LeftEncoder2.Begin(ENCODER_LEFT_2A, ENCODER_LEFT_2B, &LeftWheel.iLeftMotorRunning);       // set up left encoder
-  // RightEncoder2.Begin(ENCODER_RIGHT_2A, ENCODER_RIGHT_2B, &RightWheel.iRightMotorRunning);  // set up right encoder
-  wheelLDriveSpeed = cMaxPWM - cLeftAdjust;
-  wheelLDriveSpeed = cMaxPWM - cRightAdjust;
 
   // Set up SmartLED
   SmartLEDs.begin();                                     // initialize smart LEDs object (REQUIRED)
@@ -147,7 +127,6 @@ void setup() {
 }
 
 void loop() {
-
   long pos[] = { 0, 0 };  // current motor positions
   int pot = 0;            // raw ADC value from pot
 
@@ -171,9 +150,10 @@ void loop() {
 
     // 200 millisecond timer, counts 200 milliseconds
     timerCount200msec = timerCount200msec + 1;  // Increment 200 millisecond timer count
-    if (timerCount200msec > 200) {              // If 200 milliseconds have elapsed
-      timerCount200msec = 0;                    // Reset 200 millisecond timer count
-      timeUp200msec = true;                     // Indicate that 200 milliseconds have elapsed
+    if (timerCount200msec > 200)                // If 200 milliseconds have elapsed
+    {
+      timerCount200msec = 0;  // Reset 200 millisecond timer count
+      timeUp200msec = true;   // Indicate that 200 milliseconds have elapsed
     }
 
     // Mode pushbutton debounce and toggle
@@ -213,34 +193,23 @@ void loop() {
     switch (robotModeIndex) {
       case 0:  // Robot stopped
         Bot.Stop("D1");
-        LeftWheel.Stop("M1");
-        RightWheel.Stop("M1");
-        // clear encoder counts
-        LeftEncoder.clearEncoder();
+        LeftEncoder.clearEncoder();  // clear encoder counts
         RightEncoder.clearEncoder();
-        // LeftEncoder2.clearEncoder();
-        // RightEncoder2.clearEncoder();
         driveIndex = 0;      // reset drive index
         timeUp2sec = false;  // reset 2 second timer
         break;
 
       case 1:              // Run robot
         if (timeUp3sec) {  // pause for 3 sec before running case 1 code
-          // Read pot to update drive motor speed
+                           // Read pot to update drive motor speed
           pot = analogRead(POT_R1);
-          // wheelLDriveSpeed = map(pot, 0, 4095, cMinPWM, cMaxPWM) - cLeftAdjust;
-          // wheelRDriveSpeed = map(pot, 0, 4095, cMinPWM, cMaxPWM) - cRightAdjust;
           leftDriveSpeed = map(pot, 0, 4095, cMinPWM, cMaxPWM) - cLeftAdjust;
           rightDriveSpeed = map(pot, 0, 4095, cMinPWM, cMaxPWM) - cRightAdjust;
 #ifdef DEBUG_DRIVE_SPEED
-          Serial.print(F("Left Drive Speed: Pot R1 = "));
+          Serial.print(F(" Left Drive Speed: Pot R1 = "));
           Serial.print(pot);
           Serial.print(F(", mapped = "));
-          Serial.print(leftDriveSpeed);
-          Serial.print(F(", Left wheel speed = "));
-          Serial.print(wheelLDriveSpeed);
-          Serial.print(F(", Right wheel speed = "));
-          Serial.println(wheelRDriveSpeed);
+          Serial.println(leftDriveSpeed);
 #endif
 #ifdef DEBUG_ENCODER_COUNT
           if (timeUp200msec) {
@@ -258,9 +227,7 @@ void loop() {
             Serial.print("\n");
           }
 #endif
-          if (motorsEnabled) {                                            // run motors only if enabled
-            LeftWheel.Forward("M1", wheelLDriveSpeed, wheelRDriveSpeed);  // Spin collection wheel
-            RightWheel.Reverse("M1", wheelLDriveSpeed, wheelRDriveSpeed);
+          if (motorsEnabled) {  // run motors only if enabled
             if (timeUp2sec) {
               RightEncoder.getEncoderRawCount();  // read right encoder count
               switch (driveIndex) {               // cycle through drive states
@@ -351,7 +318,7 @@ void loop() {
                   Bot.Forward("D1", leftDriveSpeed, rightDriveSpeed);  // drive ID, left speed, right speed
 
                   if (RightEncoder.lRawEncoderCount >= target) {
-                    if (driveCounter < 100) {
+                    if (driveCounter < 1) {
                       driveCounter++;
                       driveDistance -= 10;
                       setTarget(1, RightEncoder.lRawEncoderCount, driveDistance);  // set target to drive forward
@@ -366,8 +333,6 @@ void loop() {
           }
         } else {  // stop when motors are disabled
           Bot.Stop("D1");
-          LeftWheel.Stop("M1");
-          RightWheel.Stop("M2");
         }
         break;
     }
